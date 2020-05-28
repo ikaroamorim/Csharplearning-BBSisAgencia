dfb9dd2f3b9ac55a5072d66ad7136810c8ac28be
Ikaro Amorim e Silva <ikaro.amorim@gmail.com>
2020-05-27 23:44:45 -0300

    Aprofundando sobre Writer.

--- ByteBank/ByteBank.SistemaAgencia/Program.cs
+++ ByteBank/ByteBank.SistemaAgencia/Program.cs
@@ -18,6 +18,8 @@ namespace ByteBank.SistemaAgencia
     static void Main(string[] args)
     {
       var enderecoArquivo = "contas.txt";
+      var caminhoArquivo = "contas.csv";
+
 
       using (var fluxoArquivo = new FileStream(enderecoArquivo, FileMode.OpenOrCreate))
       using (var leitor = new StreamReader(fluxoArquivo))
@@ -25,12 +27,79 @@ namespace ByteBank.SistemaAgencia
         while (!leitor.EndOfStream)
         {
           var linha = leitor.ReadLine();
+          var contaCorrente = ConvertStringCC(linha);
 
-          Console.WriteLine(linha);
+          //Console.WriteLine(linha);
+          Console.WriteLine($"Ag: {contaCorrente.Agencia} CC: {contaCorrente.Numero} Saldo:{contaCorrente.Saldo} Titular: {contaCorrente.Titular.Nome}.");
         }
       }
-      //var linha = leitor.ReadToEnd();
 
+      using (var fluxoArquivo2 = new FileStream(caminhoArquivo, FileMode.Create))
+      using (var escritor = new StreamWriter(fluxoArquivo2))
+      {
+        escritor.Write("456, 78945, 4785.50, Gustavo Santos");
+      }
+
+      CriarArquivo();
+      CriarArquivoFlushing();
+      //var linha = leitor.ReadToEnd();
+    }
+
+    static void CriarArquivoFlushing()
+    {
+      var caminhoNovoArquivo = "contasExportadasFlushing.csv";
+      using (var fluxoArquivo = new FileStream(caminhoNovoArquivo, FileMode.Create))
+      using (var escritor = new StreamWriter(fluxoArquivo))
+      {
+        for (int i = 0; i < 10000; i++)
+        {
+          escritor.WriteLine($"Linha {i}");
+
+          escritor.Flush();
+
+          Console.WriteLine($"Linha {i} foi escrita no arquivo. Tecle enter para adicionar mais uma");
+          Console.ReadLine();
+        }
+      }
+    }
+
+    static void CriarArquivo()
+    {
+      var caminhoNovoArquivo = "contasExportadas.csv";
+      using (var fluxoArquivo = new FileStream(caminhoNovoArquivo, FileMode.Create))
+      {
+        var contaComoString = "456, 78945, 4785.50, Gustavo Santos";
+        var encoding = Encoding.UTF8;
+
+        var bytes = encoding.GetBytes(contaComoString);
+
+        fluxoArquivo.Write(bytes, 0, bytes.Length);
+      }
+
+
+    }
+
+    static ContaCorrente ConvertStringCC(string linha)
+    {
+      var campos = linha.Split(',');
+
+      var agencia = campos[0];
+      var numero = campos[1];
+      var saldo = campos[2].Replace('.',',');
+      var nomeTitular = campos[3];
+
+      var intAg = int.Parse(agencia);
+      var intCC = int.Parse(numero);
+      var dbSaldo = double.Parse(saldo);
+
+      var titular = new Cliente();
+      titular.Nome = nomeTitular;
+
+      var resultado = new ContaCorrente(intAg, intCC);
+      resultado.Titular = titular;
+      resultado.Depositar(dbSaldo);
+
+      return resultado;
     }
   }
 }
